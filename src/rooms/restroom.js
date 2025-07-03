c83f43469fbe0815b335e4b3cc4d423faf4085b8
seojin2003 <seojin20031010@gmail.com>
2025-07-03 17:20:17 +0900

    화장실

---
+++ src/rooms/restroom.js
@@ -0,0 +1,23 @@
+import * as BABYLON from "@babylonjs/core";
+import "@babylonjs/loaders";
+
+/**
+ * 화장실 오브젝트(더러운 변기)를 Babylon.js 씬에 추가하는 함수
+ * @param {BABYLON.Scene} scene - Babylon.js Scene 객체
+ * @param {BABYLON.AbstractMesh} parentMesh - parent로 사용할 메시 (ex: 건물 메시)
+ */
+export async function addRestroomObject(scene, parentMesh) {
+  const result = await BABYLON.SceneLoader.ImportMeshAsync("", "/models/", "dirty_toilet.glb", scene);
+  result.meshes.forEach((mesh) => {
+    if (mesh.name !== "__root__") {
+      mesh.parent = parentMesh;
+      mesh.position = BABYLON.Vector3.TransformCoordinates(
+        new BABYLON.Vector3(-31.26, 0.8, -3.88),
+        BABYLON.Matrix.Invert(parentMesh.getWorldMatrix())
+      );
+      mesh.scaling = new BABYLON.Vector3(15, 15, 15);
+      mesh.checkCollisions = true;
+      mesh.rotationQuaternion = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.Z, -Math.PI / 2);
+    }
+  });
+}
