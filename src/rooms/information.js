9f260e78d1c064420da9b5ea5328bc02403fb9f6
seojin2003 <seojin20031010@gmail.com>
2025-07-09 15:28:41 +0900

    얼굴

--- src/rooms/information.js
+++ src/rooms/information.js
@@ -11,7 +11,7 @@ export async function addInformation(scene, parentMesh) {
 
 
     // 벽 위치 정의
-    const wallWorldPos1 = new BABYLON.Vector3(-14.42, 7, -8.70);
+    const wallWorldPos1 = new BABYLON.Vector3(-15, 7, -8.70);
     const wallWorldPos2 = new BABYLON.Vector3(-0, 7, -8.70);
     const wallWorldPos3 = new BABYLON.Vector3(-2.42, 7, -13.5);
     const wallWorldPos4 = new BABYLON.Vector3(-10.42, 7, -13.5);
@@ -239,18 +239,18 @@ const old_fridgeWorldPos = new BABYLON.Vector3(-18.5, 6.5, -11.94);
     );
   }
 
-  // 🧠 인간 뇌(human_brain.glb) 추가
-  const brainResult = await BABYLON.SceneLoader.ImportMeshAsync("", "/models/", "human_brain.glb", scene);
-  brainResult.meshes.forEach((mesh) => {
+  // 얼굴
+  const faceResult = await BABYLON.SceneLoader.ImportMeshAsync("", "/models/", "face_f2.glb", scene);
+  faceResult.meshes.forEach((mesh) => {
     if (mesh.name !== "__root__") {
       mesh.parent = parentMesh;
       mesh.position = BABYLON.Vector3.TransformCoordinates(
-        new BABYLON.Vector3(-18.51, 8.18, -11.95),
+        new BABYLON.Vector3(-18.49, 8.4, -11.95),
         BABYLON.Matrix.Invert(parentMesh.getWorldMatrix())
       );
-      mesh.scaling = new BABYLON.Vector3(5, 5, 5);
+      mesh.scaling = new BABYLON.Vector3(15, 15, 15);
       mesh.checkCollisions = true;
-      mesh.rotationQuaternion = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.X, -Math.PI / 2)
+      mesh.rotationQuaternion = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.Z, Math.PI/2)
         .multiply(BABYLON.Quaternion.RotationAxis(BABYLON.Axis.Z, Math.PI));
     }
   });
@@ -303,6 +303,7 @@ Object.values(eyeGroups).forEach((group, i) => {
     .multiply(BABYLON.Quaternion.RotationAxis(BABYLON.Axis.X, -Math.PI));
 });
 
+// 간
 const liverResult = await BABYLON.SceneLoader.ImportMeshAsync("", "/models/", "human_liver.glb", scene);
   liverResult.meshes.forEach((mesh) => {
     if (mesh.name !== "__root__") {
