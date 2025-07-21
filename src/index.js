4e297b3f9d616b53be1a1acdd8051e7716b2727b
kimsoryeong <thfud715@gmail.com>
2025-07-21 10:48:49 +0900

    메인페이지

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BabylonScene from './BabylonScene';
+import './index.css';
+import App from './App'; // <--- 여기가 <App /> 이어야 합니다.
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BabylonScene />);
+root.render(
+  <React.StrictMode>
+    <App /> {/* <--- 여기가 <App /> 이어야 합니다. */}
+  </React.StrictMode>
+);
+// ...
