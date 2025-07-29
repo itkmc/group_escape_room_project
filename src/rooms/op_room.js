7b4c879f97d8f88d7127365d8e98504a13f01d1a
kimsoryeong <thfud715@gmail.com>
2025-07-29 16:05:01 +0900

    지하 문 메세지 없애고 id카드는 안없어짐

--- src/rooms/op_room.js
+++ src/rooms/op_room.js
@@ -426,7 +426,7 @@ for (const mesh of combination_padlock.meshes) {
         if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN && kbInfo.event.key === "e" && !isUnlocked) {
             if (getHasIdCardItem()) {
                 console.log("E키로 문 열기 - ID 카드 사용됨");
-                if (onDoorInteraction) onDoorInteraction("ID 카드로 문을 열었습니다!");
+                // if (onDoorInteraction) onDoorInteraction("ID 카드로 문을 열었습니다!");
                 isUnlocked = true;
 
                 if (isAnimating) return;
